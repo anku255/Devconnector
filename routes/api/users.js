3126ec4f1c280d7e0bf3d2d3bc400615d65fbce4
Ankit Tiwari <ankitt255@gmail.com>
2018-05-16 00:04:25 +0530

    📝Add passport to handle login

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const passport = require('passport');
 const keys = require('../../config/keys');
 const User = require('../../models/User');
 
@@ -84,5 +85,20 @@ router.post('/login', (req, res) => {
     });
   });
 });
+
+// @route POST api/users/current
+// @desc Return current user
+// @access Private
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
