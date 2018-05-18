5798c9fa09fd1da9f375eb5879e69420638c98f1
Ankit Tiwari <ankitt255@gmail.com>
2018-05-18 17:41:53 +0530

    📝Add logout functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -21,6 +21,17 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currenTime = Date.now() / 1000;
+  if (decoded.exp < currenTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: Clear current profile
+
+    // Redirect to login
+    window.location.href = '/login';
+  }
 }
 
 class App extends Component {
