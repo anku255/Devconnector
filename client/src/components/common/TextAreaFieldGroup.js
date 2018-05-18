015d643dad44274ad1fcf7bd808e4f173bff349b
Ankit Tiwari <ankitt255@gmail.com>
2018-05-18 23:23:09 +0530

    📝Add custom form fields

--- client/src/components/common/TextAreaFieldGroup.js
+++ client/src/components/common/TextAreaFieldGroup.js
@@ -2,27 +2,23 @@ import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-const TextFieldGroup = ({
+const TextAreaFieldGroup = ({
   name,
   placeholder,
   value,
-  error,  
+  error,
   info,
-  type,
-  onChange,
-  disabled
+  onChange
 }) => {
   return (
     <div className="form-group">
-      <input
-        type={type}
+      <textarea
         className={classnames('form-control form-control-lg', {
           'is-invalid': error
         })}
         placeholder={placeholder}
         name={name}
         value={value}
-        disabled={disabled}
         onChange={onChange}
       />
       {info && <small className="form-text text-muted">{info}</small>}
@@ -31,19 +27,13 @@ const TextFieldGroup = ({
   );
 };
 
-TextFieldGroup.propTypes = {
+TextAreaFieldGroup.propTypes = {
   name: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
   info: PropTypes.string,
   error: PropTypes.string,
-  type: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  disabled: PropTypes.string
+  onChange: PropTypes.func.isRequired
 };
 
-TextFieldGroup.defaultProps = {
-  type: 'text'
-};
-
-export default TextFieldGroup;
+export default TextAreaFieldGroup;
