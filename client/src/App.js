b7ec69c3222852cd229f8c4892f7f6e9a0d5caf6
Ankit Tiwari <ankitt255@gmail.com>
2018-05-19 20:01:59 +0530

    📝Add EditProfile component

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-pofile/CreateProfile';
+import EditProfile from './components/edit-profile/EditProfile';
 import './App.css';
 
 // Check for token
@@ -57,6 +58,13 @@ class App extends Component {
                 exact
                 path="/create-profile"
                 component={CreateProfile}
+              />
+            </Switch>
+            <Switch>
+              <PrivateRoute
+                exact
+                path="/edit-profile"
+                component={EditProfile}
               />
             </Switch>
             <Footer />
